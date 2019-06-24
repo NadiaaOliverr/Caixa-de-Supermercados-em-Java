02cb7cced76d43ceb424d1bb49d8bdcadd3ba6c7
Luigi Miranda <luigi.miranda75@gmail.com>
2019-06-24 00:31:22 -0300

    Atualização na Tela de Edição de Funcionário

--- Código Fonte/MercadosNL/src/Visual/MercadosNLTelaEdicaoFunc.java
+++ Código Fonte/MercadosNL/src/Visual/MercadosNLTelaEdicaoFunc.java
@@ -47,7 +47,7 @@ public class MercadosNLTelaEdicaoFunc extends javax.swing.JFrame {
         BarraCargo = new javax.swing.JTextField();
         BarraCPF = new javax.swing.JTextField();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
